57c2d9dcd044a52f6a48c0706857d6fcbac76eaf
Your Name <you@example.com>
2020-01-17 10:20:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//jshint esversion:6
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Hi there");
+});
+
+app.listen(PORT,function(){
+    console.log("listening on port :"+PORT);
+})
