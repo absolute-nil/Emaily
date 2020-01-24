a3b8c80b4fd895509d0dcfef7e855b0b11e47410
Your Name <nikhil19@somaiya.edu>
2020-01-24 11:32:16 +0530

    set production setup

--- index.js
+++ index.js
@@ -35,6 +35,19 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+//for production
+if(process.env.NODE_ENV==='production'){
+  //express will server up main.css and js file
+  app.use(express.static('client/build'));
+
+  //express will server up index.html if it does not recognize path
+  const path = require('path');
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client' ,'build' ,'index.html'));
+  });
+
+}
+
 //dynamic port number of heroku and our development port specified
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
